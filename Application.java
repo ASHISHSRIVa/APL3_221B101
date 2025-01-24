ad483a95712e12ed739f1bb9633a59317de6e577
Ashish Srivastava <115929114+ASHISHSRIVa@users.noreply.github.com>
2025-01-24 09:49:26 +0530

    Application.java

--- Application.java
+++ Application.java
@@ -6,39 +6,39 @@ Welcome to GDB Online.
   Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
-// Defining the Mother class
+
 class Mother {
     int x;
     
-    // Constructor to initialize x
+    
     Mother(int x) {
         this.x = x;
     }
     
-    // Method to show the value of x
+    
     void show() {
         System.out.println("Value of x: " + x);
     }
 }
 
-// Defining the Child class that inherits from Mother
+
 class Child extends Mother {
     
-    // Constructor to initialize x in the base class
+   
     Child(int x) {
-        super(x); // Calling the constructor of the Mother class
+        super(x);
     }
 }
 
-// Application class to test the inheritance
+
 public class Application {
     public static void main(String args[]) {
-        // Creating an object of Mother class
-        Mother m = new Mother(10);
-        m.show(); // show of Mother is called
         
-        // Creating an object of Child class
+        Mother m = new Mother(10);
+        m.show(); 
+        
+      
         Child ch = new Child(20);
-        ch.show(); // show() inherited in Child from Mother is called
+        ch.show(); 
     }
 }
