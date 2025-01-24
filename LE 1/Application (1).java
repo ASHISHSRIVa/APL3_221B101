774fd84b6bbdea93c8b92b973a1eca47b60569fe
Ashish Srivastava <115929114+ASHISHSRIVa@users.noreply.github.com>
2025-01-24 10:30:50 +0530

    Update Application (1).java

--- LE 1/Application (1).java
+++ LE 1/Application (1).java
@@ -6,45 +6,35 @@ Welcome to GDB Online.
   Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
-// Defining the Mother class
+
 class Mother {
     int x;
-    
-    // Constructor to initialize x
+ 
     Mother(int x) {
         this.x = x;
     }
-    
-    // Method to show a message
+  
     void show() {
         System.out.println("Hello World");
     }
 }
 
-// Defining the Child class that inherits from Mother
 class Child extends Mother {
-    
-    // Constructor to initialize x in the base class
+
     Child(int x) {
-        super(x); // Calling the constructor of the Mother class
+        super(x); 
     }
-    
-    // Overriding the show method
-    @Override
+  
     void show() {
         System.out.println("Hello JUET");
     }
 }
-
-// Application class to test the inheritance and method overriding
 public class Application {
     public static void main(String args[]) {
-        // Creating an object of Mother class
+    
         Mother m = new Mother(10);
-        m.show(); // show of Mother is called, should display "Hello World"
-        
-        // Creating an object of Child class
+        m.show(); 
         Child ch = new Child(20);
-        ch.show(); // show() overridden in Child from Mother is called, should display "Hello JUET"
+        ch.show(); 
     }
 }
